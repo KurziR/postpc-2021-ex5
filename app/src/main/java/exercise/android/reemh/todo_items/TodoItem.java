e7f4ba1d54d821b183abc7b789f6061abdc51327
reut <reut.kurzweil@mail.huji.ac.il>
2021-05-19 13:50:25 +0300

    start the project

--- app/src/main/java/exercise/android/reemh/todo_items/TodoItem.java
+++ app/src/main/java/exercise/android/reemh/todo_items/TodoItem.java
@@ -4,4 +4,12 @@ import java.io.Serializable;
 
 public class TodoItem implements Serializable {
   // TODO: edit this class as you want
+
+    enum status {
+        IN_PROGRESS,
+        DONE
+    }
+
+    status curr_status = status.IN_PROGRESS;
+    String task_name;
 }
--- app/src/main/java/exercise/android/reemh/todo_items/TodoItemsHolderImpl.java
+++ app/src/main/java/exercise/android/reemh/todo_items/TodoItemsHolderImpl.java
@@ -5,17 +5,37 @@ import java.util.List;
 // TODO: implement!
 public class TodoItemsHolderImpl implements TodoItemsHolder {
   @Override
-  public List<TodoItem> getCurrentItems() { return null; }
+  public List<TodoItem> getCurrentItems() {
+    return toDoesList;
+  }
 
   @Override
-  public void addNewInProgressItem(String description) { }
+  public void addNewInProgressItem(String description) {
+    TodoItem new_item = new TodoItem();
+    new_item.task_name = description;
+    toDoesList.add(0, new_item); // add the item in the beginning of the list
+  }
 
   @Override
-  public void markItemDone(TodoItem item) { }
+  public void markItemDone(TodoItem item) {
+    item.curr_status = TodoItem.status.DONE;
+  }
 
   @Override
-  public void markItemInProgress(TodoItem item) {}
+  public void markItemInProgress(TodoItem item) {
+    item.curr_status = TodoItem.status.IN_PROGRESS;
+  }
 
   @Override
-  public void deleteItem(TodoItem item) { }
+  public void deleteItem(TodoItem item) {
+    try {
+      for (int i = 0; i < toDoesList.size(); i++) {
+        if (item.equals(toDoesList.get(i))) {
+          toDoesList.remove(toDoesList.get(i));
+        }
+      }
+    } catch (NullPointerException e) {
+      return;
+    }
+  }
 }
